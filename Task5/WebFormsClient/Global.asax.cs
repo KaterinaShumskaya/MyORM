37c5d612b4771deaf9fd5cfa0a86cde30fefccba
KaterinaShumskaya <Kate-beauty@mail.ru>
2014-04-24 11:14:37 +0400

    Inject dependencies with IHttpModule (works for routing)

--- Task5/WebFormsClient/Global.asax.cs
+++ Task5/WebFormsClient/Global.asax.cs
@@ -5,14 +5,21 @@ using System.Web.Routing;
 
 namespace WebFormsClient
 {
+    using Castle.Windsor;
+    using Castle.Windsor.Installer;
+
     public class Global : HttpApplication
     {
+        private static IWindsorContainer _container;
+
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterOpenAuth();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            _container = new WindsorContainer();
+            _container.Install(FromAssembly.This());
         }
 
         void Application_End(object sender, EventArgs e)
@@ -24,6 +31,16 @@ namespace WebFormsClient
         {
             // Code that runs when an unhandled error occurs
 
+        }
+
+        internal static object Resolve(Type type)
+        {
+            return _container.Resolve(type);
+        }
+
+        internal static void Release(object component)
+        {
+            _container.Release(component);
         }
 
         public void Load()
---
+++ Task5/WebFormsClient/WindsorHttpModule.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFormsClient
+{
+    using System.Reflection;
+    using System.Web.UI;
+
+    public class WindsorHttpModule : IHttpModule
+    {
+        private HttpApplication _context;
+
+        public void Init(HttpApplication context)
+        {
+            _context = context;
+            _context.PreRequestHandlerExecute += InjectDependencies;
+        }
+
+        private static object GetInstance(Type type)
+        {
+            return Container.GetContainer().Resolve(type);
+        }
+
+        private  void InjectDependencies(object sender, EventArgs e)
+        {
+            var page = _context.Context.CurrentHandler as Page;
+                if (page != null)
+                {
+                    Type pageType = page.GetType().BaseType;
+
+                    var ctor = GetInjectableCtor(pageType);
+
+                    if (ctor != null)
+                    {
+                        object[] arguments =
+                            (from parameter in ctor.GetParameters() select GetInstance(parameter.ParameterType)).ToArray();
+
+                        ctor.Invoke(page, arguments);
+                    }
+                }
+        }
+
+        private static ConstructorInfo GetInjectableCtor(Type type)
+        {
+            var overloadedPublicConstructors = (
+                from constructor in type.GetConstructors()
+                where constructor.GetParameters().Length > 0
+                select constructor).ToArray();
+
+            if (overloadedPublicConstructors.Length == 0)
+            {
+                return null;
+            }
+
+            if (overloadedPublicConstructors.Length == 1)
+            {
+                return overloadedPublicConstructors[0];
+            }
+
+            throw new Exception(string.Format(
+                "The type {0} has multiple public " +
+                "ctors and can't be initialized.", type));
+        }
+    
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
